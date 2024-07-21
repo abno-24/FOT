ae20f21921c18ea04be4f16c6eaa078f4577ff92
Abno <abhinavmaurya605@gmail.com>
2024-07-21 23:36:28 +0530

    code refactored

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="app-header">
+      <nav>
+        <div className="logo-bx"></div>
+        <div className="nav-links">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
