f9d821d10b38d03292c1866978cacb837f6ac417
Abno <abhinavmaurya605@gmail.com>
2024-07-21 23:41:30 +0530

    add: tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ const Header = () => {
   return (
     <header className="app-header">
       <nav>
-        <div className="logo-bx"></div>
+        <div className="logo-bx">
+          <h1 className="text-3xl font-bold underline text-red-400">Hello</h1>
+        </div>
         <div className="nav-links">
-          <ul>
+          <ul className="text-3xl font-bold underline text-red-400">
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
