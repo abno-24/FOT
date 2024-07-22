a9a42982f773446a69b048c8d7990d326586ee2b
Abno <abhinavmaurya605@gmail.com>
2024-07-22 20:26:26 +0530

    add: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,42 @@
 const Header = () => {
+  const link = [
+    {
+      id: 1,
+      name: "Home",
+      to: "#",
+    },
+    {
+      id: 2,
+      name: "About",
+      to: "#",
+    },
+    {
+      id: 3,
+      name: "Contact",
+      to: "#",
+    },
+  ];
+
   return (
-    <header className="app-header">
-      <nav>
-        <div className="logo-bx">
-          <h1 className="text-3xl font-bold underline text-red-400">Hello</h1>
+    <header className="shadow">
+      <nav className="max-container flex justify-between items-center py-2">
+        <div className="text-center">
+          <h1 className="custom-font text-2xl capitalize font-bold">
+            Food on <br />
+            <span>time 🍜</span>
+          </h1>
         </div>
-        <div className="nav-links">
-          <ul className="text-3xl font-bold underline text-red-400">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+        <div>
+          <ul className="text-xl flex justify-between items-center gap-12 tracking-widest">
+            {link.map((link) => (
+              <li key={link.id}>{link.name}</li>
+            ))}
           </ul>
+        </div>
+        <div className="cart-btn-bx">
+          <span className="text-3xl material-symbols-outlined">
+            shopping_cart
+          </span>
         </div>
       </nav>
     </header>
